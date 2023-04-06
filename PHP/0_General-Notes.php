4f4361010e4d02fc572e1faa430b5cb00acd602d
Jasiel Chao <jasielchao@outlook.com>
2023-04-06 17:14:00 -0400

    add

--- PHP/0_General-Notes.php
+++ PHP/0_General-Notes.php
@@ -14,12 +14,27 @@
     </pre>
 </div>
 
-<!-- Substring PHP -->
-<?php 
-   /* The substr() function returns a part of a string.
-      substr(string,start,length)                        */
-      echo substr("Hello world",0 ,6);
-?>
+<!-- String and Substring PHP -->
+<div>
+    <!-- String  Functions-->
+    <?php
+        # To Convert String into an Array
+        $string_array = str_split($string);
+
+        //To Replace 
+        echo str_replace("Old","New","This is Old!");
+        
+        /* To convert to LowerCase a string */
+        echo strtolower($string_variable);
+    ?>
+
+    <!-- Substring Functions -->
+    <?php
+        /* The substr() function returns a part of a string.
+        substr(string,start,length)                        */
+        echo substr("Hello world",0 ,6);
+    ?>
+</div>
 
 <!-- Convert String into an Array PHP -->
 <?php 
@@ -27,8 +42,6 @@
     //To Replace
     echo str_replace("world","Peter","Hello world!");
 ?>
-
-
 
 <!-- Foreach -->
 <?php 
