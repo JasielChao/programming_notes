b10675d14d46b853250485757e5664ded0d0785f
JasielCh <jasielchao@outlook.com>
2023-04-25 14:03:10 -0400

    add Event Scroll

---
+++ Scripts/back-to-top-button/script.js
@@ -0,0 +1,21 @@
+
+/* Event Scroll to Back Top Button*/
+document.addEventListener('DOMContentLoaded', ()=>{
+
+  let backTop = $('#backTop');
+  let oldScrollY = window.scrollY;
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300 && oldScrollY > window.scrollY) {
+      backTop.addClass('visible');
+    } else {
+      backTop.removeClass('visible');
+    }
+    oldScrollY = window.scrollY;
+  });
+  
+  backTop.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+});
