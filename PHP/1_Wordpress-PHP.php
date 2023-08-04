5f6a53b3eb61a3262502abaf98b6c09501291a9f
Jasiel Chao <jasielchao@outlook.com>
2023-08-04 17:00:48 -0400

    add

--- PHP/1_Wordpress-PHP.php
+++ PHP/1_Wordpress-PHP.php
@@ -3,6 +3,9 @@
 <!-- Get the Current Route Source Dynamically -->
 <?php echo get_template_directory_uri();?>
 
+<!-- To include a partial file-->
+<?php include get_template_directory() . '/template-parts/cognitos/cognito-form-1.php' ?>
+
 <!-- Wordpress Query  -->   
 <div>
     <!-- First Way  -->   
